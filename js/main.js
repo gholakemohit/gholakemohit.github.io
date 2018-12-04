4cab0a9072dd7d5ebe36aaff0aa94f6f0ac69658
gholakemohit <gholakemohit@gmail.com>
2018-12-04 20:05:46 +0530

    First commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	$('ul.tabs').tabs({
+		swipeable: true,
+		responsiveThreshold: 2020
+	});
+});
+
+
+$(document).ready(function(){
+  $('.tap-target').tapTarget('open');
+});
+
+  $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+
+
+ $(document).ready(function(){
+   $('.sidenav').sidenav();
+  });
+
+  $(document).ready(function(){
+    $('.tap-target').tapTarget();
+  });
+       
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+        
